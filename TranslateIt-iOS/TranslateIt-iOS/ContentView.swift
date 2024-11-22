ac4ecf5089bfcf927530cfd7d4190010c4232538
Allan <allan.viana2003@gmail.com>
2024-11-22 04:33:25 -0300

    Implemented WelcomeView layout

--- TranslateIt-iOS/TranslateIt-iOS/ContentView.swift
+++ TranslateIt-iOS/TranslateIt-iOS/ContentView.swift
@@ -9,13 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { geometry in
+            ZStack(alignment: .bottom) {
+                Color(.paleGreen)
+                    .ignoresSafeArea()
+                
+                VStack {
+                    HStack {
+                        Text("TranslateIt")
+                            .font(.custom("PressStart2P-Regular", size: 14))
+                            .foregroundStyle(.darkForestGreen)
+                        Spacer()
+                    }
+                                        
+                    HStack{
+                        Text("Bem-vindo!")
+                            .font(.custom("PressStart2P-Regular", size: 32))
+                            .foregroundStyle(.darkForestGreen)
+                        Spacer()
+                    }.padding(.top, 90)
+                    
+                    HStack {
+                        Text("Adivinhe a traducao correta da palavra em ingles e ganhe pontos!")
+                            .font(.custom("PressStart2P-Regular", size: 14))
+                            .foregroundStyle(.mintGreen)
+                            .lineSpacing(8)
+                        Spacer()
+                    }.padding(.top, 20)
+                                        
+                    Image("WelcomeImage")
+                        .scaledToFit()
+                        .padding(.top, geometry.size.height * 0.13)
+                    Spacer()
+                                        
+                }.padding(.horizontal, 30).zIndex(2)
+              
+                VStack {
+                    Button(action: {
+                        
+                    }
+                    ){
+                        Text("Jogar")
+                            .font(.custom("", size: 18 ))
+                            .foregroundStyle(Color.white)
+                           
+                            .frame(width: 280,height: 50)
+                            .background(.darkForestGreen)
+                            .clipShape(RoundedRectangle(cornerRadius: 100))
+                            
+                    }.padding(.bottom,40 )
+                    
+   
+                }.frame(maxWidth: .infinity).frame(
+                        height: geometry.size.height * 0.35, alignment: .bottom).background(.limeGlow)
+            }.frame(maxWidth: .infinity, maxHeight: .infinity)
         }
-        .padding()
     }
 }
 
