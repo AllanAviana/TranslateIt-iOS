623ad2427f505be952d68d7d93eb4f41d9666dff
Allan <allan.viana2003@gmail.com>
2024-11-22 03:04:48 -0300

    Initial commit

---
+++ TranslateIt-iOS/TranslateIt-iOS/TranslateIt_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateIt_iOSApp.swift
+//  TranslateIt-iOS
+//
+//  Created by Allan Viana on 22/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TranslateIt_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TranslateIt-iOS/TranslateIt-iOSTests/TranslateIt_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateIt_iOSTests.swift
+//  TranslateIt-iOSTests
+//
+//  Created by Allan Viana on 22/11/24.
+//
+
+import Testing
+@testable import TranslateIt_iOS
+
+struct TranslateIt_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
